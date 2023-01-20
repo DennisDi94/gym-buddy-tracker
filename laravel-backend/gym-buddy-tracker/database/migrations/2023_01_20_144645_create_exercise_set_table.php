41aa0bfebb3c782238dcc305db51d41cffb69685
Dennis <dennismaxdisselhoff@gmail.com>
2023-01-20 16:31:59 +0100

    Backend added migrations and models

---
+++ laravel-backend/gym-buddy-tracker/database/migrations/2023_01_20_144645_create_exercise_set_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercise_set', function (Blueprint $table) {
+            $table->id();
+            $table->string("weight");
+            $table->string("reps");
+            $table->foreignId('exercise_id')->constrained('exercises');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercise_set');
+    }
+};
